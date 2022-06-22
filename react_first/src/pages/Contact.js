1dae28c890811f47b3d762eea3c988b3edd0bfea
antonio Silva <antonios@uoregon.edu>
2022-06-21 22:46:21 -0700

    mail added

--- react_first/src/pages/Contact.js
+++ react_first/src/pages/Contact.js
@@ -32,9 +32,8 @@ function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
-                 antonios@uoregon.edu
-              </a>
+              <a className="text-indigo-400 leading-relaxed" href="mailto:antonios@uoregon.edu">
+                 antonios@uoregon.edu</a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
